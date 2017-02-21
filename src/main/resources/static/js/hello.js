2a959b122c514e85a60e05e5a8c3394ece347890
Daniel Martinez <roanhawkeye@gmail.com>
2017-02-21 16:51:43 -0600

    Implements navitaions controller and authentication on client side

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -20,4 +20,38 @@ angular.module('hello', ['ngRoute'])
     	self.greeting = response.data;
     }) 
   })
-  .controller('navigation', function(){});
+  .controller('navigation', function($rootScope, $http, $location){
+	  var self = this;
+	  
+	  var authenticate = function(credentials, callback){
+		  var headers = credentials ? { authorization : "Basic " + btoa(credentials.username + ":" + credentials.password)} : {};
+		  
+		  $htpp.get('user', {headers : headers}).then(function(response){
+			  if(response.data.name){
+				  $rootScope.authenticated = true;
+			  }else{
+				  $rootScope.authenticated = false;
+			  }
+			  callback && callback();
+		  }, function(){
+			  $rootScope.authenticated = false;
+			  callback && callback();
+		  })
+	  }
+	  
+	  authenticate();
+	  
+	  self.credentials = {};
+	  self.login = function(){
+		  authenticate(self.credentials, function(){
+			  if($rootScope.authenticated){
+				  $location.path("/");
+				  self.error = false;
+			  }else{
+				  $location.path("/login");
+				  self.error = true;
+			  }
+		  })
+	  };
+	  
+  });
