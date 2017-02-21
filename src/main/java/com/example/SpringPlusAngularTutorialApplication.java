4d6386d954c9a226dd0277640d91921f241407e6
Daniel Martinez <roanhawkeye@gmail.com>
2017-02-21 17:26:18 -0600

    Implements security backend logic

--- src/main/java/com/example/SpringPlusAngularTutorialApplication.java
+++ src/main/java/com/example/SpringPlusAngularTutorialApplication.java
@@ -1,17 +1,28 @@
 package com.example;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SpringPlusAngularTutorialApplication {
+	
+	@RequestMapping("/user")
+	public Principal user(Principal user){
+		return user;
+	}
 	
 	@RequestMapping("/resource")
 	public Map<String, Object> home(){
@@ -21,6 +32,19 @@ public class SpringPlusAngularTutorialApplication {
 	    return model;
 	}
 	
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+		
+		protected void configure(HttpSecurity http) throws Exception{
+			http
+				.httpBasic()
+			.and()
+			  	.authorizeRequests()
+			  		.antMatchers("/index.html","/home.html","/login.html","/").permitAll()
+			  		.anyRequest().authenticated();
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPlusAngularTutorialApplication.class, args);
