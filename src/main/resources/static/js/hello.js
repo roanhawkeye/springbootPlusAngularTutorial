0e831eb00fb32c1d612ce3a94de3ce0d63543676
Daniel Martinez <roanhawkeye@gmail.com>
2017-02-21 15:48:13 -0600

    Fetch data from backend on hello controller angular

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,4 +1,8 @@
 angular.module('hello', [])
-  .controller('home', function() {
-    this.greeting = {id: 'xxx', content: 'Hello World!'}
+  .controller('home', function($http) {
+    var self = this;
+    $http.get('/resource/').then(function(response){
+    	self.greeting = response.data;
+    })
+	  
 })
