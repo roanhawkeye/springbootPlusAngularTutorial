8d91f25bbd31ba09bf093c2ccf95998a356e0772
Daniel Martinez <roanhawkeye@gmail.com>
2017-02-21 16:31:21 -0600

    Config ngroutes and add navitagion controller

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,8 +1,23 @@
-angular.module('hello', [])
+angular.module('hello', ['ngRoute'])
+  .config(function($routeProvider, $httpProvider){
+	
+	  $routeProvider.when('/',{
+		  templateUrl: 'home.html',
+		  controller: 'home',
+		  controllerAs: 'controller'
+	  }).when('/login', {
+		  templateUrl: 'login.html',
+		  controller: 'navigation',
+		  controllerAs: 'controller'
+	  }).otherwise('/');
+	  
+	  $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+	  
+  })
   .controller('home', function($http) {
     var self = this;
     $http.get('/resource/').then(function(response){
     	self.greeting = response.data;
-    })
-	  
-})
+    }) 
+  })
+  .controller('navigation', function(){});
