759ad399f506fc0d351a5e0d2f64ff3e6a5b5ac1
Daniel Martinez <dmartinez@itexico.net>
2017-02-22 09:23:16 -0600

    Change securite configuration to send token for CSRF

--- src/main/java/com/example/SpringPlusAngularTutorialApplication.java
+++ src/main/java/com/example/SpringPlusAngularTutorialApplication.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,7 +43,10 @@ public class SpringPlusAngularTutorialApplication {
 			.and()
 			  	.authorizeRequests()
 			  		.antMatchers("/index.html","/home.html","/login.html","/").permitAll()
-			  		.anyRequest().authenticated();
+			  		.anyRequest().authenticated()
+			.and()
+			  	.csrf()
+			  		.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 		}
 	}
 
