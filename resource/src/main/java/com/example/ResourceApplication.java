5bd5029f2aeec4deb0d548a8848c35f0ef828400
Daniel Martinez <dmartinez@itexico.net>
2017-02-22 10:17:36 -0600

    Allow CORS for the UIserver

--- resource/src/main/java/com/example/ResourceApplication.java
+++ resource/src/main/java/com/example/ResourceApplication.java
@@ -4,12 +4,16 @@ import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ResourceApplication {
 	
 	@RequestMapping("/")
+	@CrossOrigin(origins="*", maxAge=3600)
 	public Message home(){
 		return new Message("Hello World");
 	}
