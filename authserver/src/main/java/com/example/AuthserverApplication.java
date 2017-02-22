5bde1e0334fcebb1d269ecffb48b046af9286ce2
Daniel Martinez <roanhawkeye@gmail.com>
2017-02-22 16:16:29 -0600

    Add initial configuration for auth server

--- authserver/src/main/java/com/example/AuthserverApplication.java
+++ authserver/src/main/java/com/example/AuthserverApplication.java
@@ -2,9 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class AuthserverApplication {
+@EnableAuthorizationServer
+public class AuthserverApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthserverApplication.class, args);
