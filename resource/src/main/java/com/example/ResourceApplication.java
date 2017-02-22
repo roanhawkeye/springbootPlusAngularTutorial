ba66cc55319dd9b76409df7dd695ee4deed0ce67
Daniel Martinez <dmartinez@itexico.net>
2017-02-22 11:59:53 -0600

    Allow resource server to accept custom headers

--- resource/src/main/java/com/example/ResourceApplication.java
+++ resource/src/main/java/com/example/ResourceApplication.java
@@ -4,18 +4,25 @@ import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class ResourceApplication {
+public class ResourceApplication extends WebSecurityConfigurerAdapter{
 	
 	@RequestMapping("/")
-	@CrossOrigin(origins="*", maxAge=3600)
+	@CrossOrigin(origins="*", maxAge=3600, allowedHeaders={"x-auth-token", "x-requested-with"})
 	public Message home(){
 		return new Message("Hello World");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.cors().and().authorizeRequests().anyRequest().authenticated();
 	}
 
 	public static void main(String[] args) {
