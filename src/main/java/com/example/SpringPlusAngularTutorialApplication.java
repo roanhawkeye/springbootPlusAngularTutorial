c42db776928de5039d837f83b18afcfb1ec8a933
Daniel Martinez <dmartinez@itexico.net>
2017-02-22 12:40:37 -0600

    Fix dependencies for properly proxy configuration

--- src/main/java/com/example/SpringPlusAngularTutorialApplication.java
+++ src/main/java/com/example/SpringPlusAngularTutorialApplication.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
